9bbc00b301d040532237b5bd87fa0a75c3f2d2b9
JEBIN AKASH RAJA A <165483288+akashraja83@users.noreply.github.com>
2025-08-05 16:14:04 +0530

    Time: 3 ms (45.78%) | Memory: 8.7 MB (25.82%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        long reverse = 0,original = x;
+        while(x!=0){
+            int digit = x%10;
+            reverse = reverse * 10 + digit;
+            x/=10;
+        }
+        return original==reverse;
+    }
+};
