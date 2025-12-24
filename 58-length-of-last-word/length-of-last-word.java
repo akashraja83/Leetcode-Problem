f512d2e470ae231d00968a5fd11ff366c0be978c
JEBIN AKASH RAJA A <165483288+akashraja83@users.noreply.github.com>
2025-12-24 09:15:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (11.74%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -2,8 +2,8 @@ class Solution {
     public int lengthOfLastWord(String s) {
         s = s.trim();
         int last = s.lastIndexOf(' ');
-        if(last == -1)
-            return s.length();
+        // if(last == -1)
+        //     return s.length();
         return s.length()-last-1;
     }
 }
