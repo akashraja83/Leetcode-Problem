461272d82803e3ceb5166d84d21e5aabad97b05f
JEBIN AKASH RAJA A <165483288+akashraja83@users.noreply.github.com>
2026-01-02 18:29:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (12.47%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -1,20 +1,19 @@
 class Solution {
     public int numberOfMatches(int n) {
-        // int team = n;
-        // int matches;
-        // int sum =0;
-        // while(team>1){
-        //     if(team%2==0){
-        //         matches=team/2;
-        //         team = team/2;
-        //     }
-        //     else{
-        //         matches = (team-1)/2;
-        //         team = (team-1)/2+1;
-        //     }
-        //     sum+=matches;
-        // }
-        // return sum;
-        return n-1;
-       }
+        int team = n;
+        int matches;
+        int sum =0;
+        while(team>1){
+            if(team%2==0){
+                matches=team/2;
+                team = team/2;
+            }
+            else{
+                matches = (team-1)/2;
+                team = (team-1)/2+1;
+            }
+            sum+=matches;
+        }
+        return sum;
+    }
 }
