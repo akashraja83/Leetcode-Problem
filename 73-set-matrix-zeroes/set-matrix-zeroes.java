20ba96150542dc3bb248931a32ab6e4df9b0f878
JEBIN AKASH RAJA A <165483288+akashraja83@users.noreply.github.com>
2026-01-20 09:01:53 +0530

    Time: 1 ms (58.94%) | Memory: 47.6 MB (21.11%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -6,24 +6,18 @@ class Solution {
 
         boolean firstRowZero = false;
         boolean firstColZero = false;
-
-        // check first row
         for (int j = 0; j < n; j++) {
             if (matrix[0][j] == 0) {
                 firstRowZero = true;
                 break;
             }
         }
-
-        // check first column
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] == 0) {
                 firstColZero = true;
                 break;
             }
         }
-
-        // mark zeros
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
@@ -32,8 +26,6 @@ class Solution {
                 }
             }
         }
-
-        // set zeros using markers
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
@@ -41,15 +33,11 @@ class Solution {
                 }
             }
         }
-
-        // handle first row
         if (firstRowZero) {
             for (int j = 0; j < n; j++) {
                 matrix[0][j] = 0;
             }
         }
-
-        // handle first column
         if (firstColZero) {
             for (int i = 0; i < m; i++) {
                 matrix[i][0] = 0;
