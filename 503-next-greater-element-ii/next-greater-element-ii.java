2b75c94b9bb13144324a1ce09c3102b45e755ff0
JEBIN AKASH RAJA A <165483288+akashraja83@users.noreply.github.com>
2026-02-23 12:11:52 +0530

    Time: 9 ms (89.04%) | Memory: 48.7 MB (31.42%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -4,15 +4,14 @@ class Solution {
         int[] res = new int[n];
         Arrays.fill(res, -1);
         Stack<Integer> stack = new Stack<>();
-        for(int i=2*n-1;i>=0;i--){
-            int curr = nums[i%n];
-            while(!stack.isEmpty() && stack.peek() <= curr){
-                stack.pop();
+        for (int i=0;i<2*n;i++) {
+            int curr = nums[i % n];
+            while(!stack.isEmpty() && nums[stack.peek()] < curr) {
+                res[stack.pop()] = curr;
             }
-            if(i<n && !stack.isEmpty()){
-                res[i] = stack.peek();
+            if(i<n){
+                stack.push(i);
             }
-            stack.push(curr);
         }
         return res;
     }
