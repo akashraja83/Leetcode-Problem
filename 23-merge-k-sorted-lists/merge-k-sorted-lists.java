4b870b350558e66b97f32af62d25723f40bf03da
JEBIN AKASH RAJA A <165483288+akashraja83@users.noreply.github.com>
2026-05-06 14:21:42 +0530

    Time: 4 ms (80.29%) | Memory: 47.1 MB (5.36%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,39 +10,22 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) {
-            return null;
-        }
-
-        while (lists.length > 1) {
-            List<ListNode> temp = new ArrayList<>();
-            for (int i = 0; i < lists.length; i += 2) {
-                ListNode l1 = lists[i];
-                ListNode l2 = i + 1 < lists.length ? lists[i + 1] : null;
-                temp.add(mergeLists(l1, l2));
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)-> a.val - b.val);
+        for(ListNode node : lists){
+            if(node != null){
+                pq.add(node);
             }
-            lists = temp.toArray(new ListNode[0]);
         }
-
-        return lists[0];        
-    }
-
-    private ListNode mergeLists(ListNode l1, ListNode l2) {
-        ListNode node = new ListNode();
-        ListNode ans = node;
-
-        while (l1 != null && l2 != null) {
-            if (l1.val > l2.val) {
-                node.next = l2;
-                l2 = l2.next;
-            } else {
-                node.next = l1;
-                l1 = l1.next;
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while(!pq.isEmpty()){
+            ListNode Node = pq.poll();
+            tail.next = Node;
+            tail = tail.next;
+            if(Node.next != null){
+                pq.add(Node.next);
             }
-            node = node.next;
         }
-
-        node.next = l1 != null ? l1 : l2;
-        return ans.next;
+        return dummy.next;
     }    
 }
